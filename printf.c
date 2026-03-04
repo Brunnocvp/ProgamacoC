03cf6483d156535b8ea75ce45d96f4128e456a8d
Brunnocvp <brunnocvp@gmail.com>
2026-03-04 14:01:10 -0300

    Create printf.c

---
+++ printf.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+
+int main(){
+
+  //sintaxe padrao printf
+  printf("olá mundo!");
+  //printf("texto com formataçao", variavel, variavel2, ...);
+  //printf("ola mundo!\n");
+
+}
