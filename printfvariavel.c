42d2f7b246ad9f328e5ac2ac76a023c804b5e6b3
Brunnocvp <brunnocvp@gmail.com>
2026-03-04 14:21:12 -0300

    Create printfvariavel.c

---
+++ printfvariavel.c
@@ -0,0 +1,23 @@
+#include <studio.h>
+
+int main(){
+  int idade = 25;
+  float altura = 1,75;
+  char opçao = 's';
+  char nome[20] = brunno;
+
+  printf("a idade do %s é: %d\n", nome, idade);
+  printf("a atura é: %e\n, altura);
+  printf("a opçao é: %c\n", opçao);
+
+  /*
+  printf("%formato1 %formato2", variável1, variável2);
+
+  %d imprime um inteiro no formato decimal
+  %i quivale a %d
+  %f imprme um numero de ponto flutuante no formato normal.
+  %e imprime um numero de ponto flutuante na notaçao cientifica
+  %c imprime um unico caractere
+  %s imprime uma cadeia(string) de caracteres
+  *\
+}
